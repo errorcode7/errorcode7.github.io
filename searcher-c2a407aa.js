0d5ff3952d8a5e2b35bfe50c5b2bd02d183fca41
errorcode7 <errorcode7@users.noreply.github.com>
2025-11-30 18:17:46 +0000

    deploy: 1b9222bc0052f8241298b1516318ed1ac3d755fc

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f2a8a5fe.js',
+                path_to_root + 'searchindex-56aa291c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
